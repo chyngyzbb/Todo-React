4ade0e4a56270b01f322954a6ccc71df4be1f1f4
chyngyzbb <c91618645@gmail.com>
2024-12-20 21:31:33 +0600

    add api

--- src/component/Todo/index.js
+++ src/component/Todo/index.js
@@ -1,60 +1,145 @@
-import React, { useState } from 'react';
-import uniqId from 'uniqid'
-import TodoList from '../TodoList';
-
+import React, { useEffect, useState } from "react";
+import uniqId from "uniqid";
+import TodoList from "../TodoList";
 
 const Todo = () => {
+  const [value, setValue] = useState("");
+  const [todos, setTodos] = useState([]);
 
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
 
-
-    const [value,setValue]=useState('')
-    const [todos,setTodos]=useState([])
-
-    const handleChange=(e)=>{
-        setValue(e.target.value)
-    }
-
-    const handleClick=()=>{
-        const newTodo={
-            id:uniqId(),
-            title:value,
-            isDone:false
-        }
-        setTodos([...todos,newTodo])
-    }
-
-    const deleteTodo=(id)=>{
-        setTodos(todos.filter(el=>el.id!==id))
-    }
-
-
-    return (
-        <div>
-            <div className='w-[500px] flex gap-1 my-[50px] mx-auto items-center' >
-            <input 
-            onChange={handleChange}
-            type="search" 
-            id="default-search"
-            className="block w-full p-4 outline-none py-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-blue-100 focus:ring-blue-500  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-             required />
-
-            <button 
-            onClick={handleClick}
-            type="button" 
-            className="text-white h-[52px] my-2  bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br  dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-            >Add</button>
-
-            </div>
-            {
-                todos.map((el)=>(
-                    <div className='w-[500px] bg-amber-400 py-4 px-3 mx-auto rounded-1 '>
-                            <TodoList el={el} key={el.id} deleteTodo={deleteTodo}/>
-                    </div>
-                ))
-            }
-            
-        </div>
+  const getAllTasks = async () => {
+    const response = await fetch(
+      "https://6765634852b2a7619f5f643f.mockapi.io/Task"
     );
+    const data = await response.json();
+    setTodos(data);
+  };
+
+  const handleClick = async () => {
+    const newTodo = {
+      title: value,
+    };
+    // setTodos([...todos,newTodo])
+    try {
+      await fetch("https://6765634852b2a7619f5f643f.mockapi.io/Task", {
+        methot: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newTodo),
+      });
+      getAllTasks();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const deleteTodo = async (id) => {
+    // setTodos(todos.filter(el=>el.id!==id))
+    try {
+      await fetch("https://6765634852b2a7619f5f643f.mockapi.io/Task/" + id, {
+        methot: "DELETE",
+      });
+      getAllTasks();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const updateTodo = async (id, newTitle) => {
+    // setTodos(
+    //     todos.map((el)=>(el.id===id?{...el,title:newTitle}:el)))
+    const upDateTodo = {
+      title: newTitle,
+    };
+    try {
+      await fetch(`https://6765634852b2a7619f5f643f.mockapi.io/Task/${id}`, {
+        methot: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(upDateTodo),
+      });
+      getAllTasks();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const updateStatus = async (id) => {
+    // setTodos(
+    //     todos.map((el)=>(el.id===id?{...el,isDone: !el.isDone}:el))
+    // )
+    const found = todos.find((el) => el.id === id);
+    const updateStatus = {
+      isDone: !found.isDone,
+    };
+    try {
+      await fetch(`https://6765634852b2a7619f5f643f.mockapi.io/Task/${id}`, {
+        methot: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updateStatus),
+      });
+      getAllTasks();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getAllTasks();
+  }, []);
+  console.log(todos);
+
+  return (
+    <div>
+      <div className="w-[500px] flex gap-1 my-[50px] mx-auto items-center">
+        <input
+          onChange={handleChange}
+          type="search"
+          id="default-search"
+          className="block w-full p-4 outline-none py-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-blue-100 focus:ring-blue-500  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          required
+        />
+
+        <button
+          onClick={handleClick}
+          type="button"
+          className="text-white h-[52px] my-2  bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br  dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+          Add
+        </button>
+      </div>
+      {todos
+        .filter((el) => !el.isDone)
+        .map((el) => (
+          <TodoList
+            updateStatus={updateStatus}
+            updateTodo={updateTodo}
+            el={el}
+            key={el.id}
+            deleteTodo={deleteTodo}
+          />
+        ))}
+      <h1 className="text-center mt-6">Законченные дела:</h1>
+      {todos
+        .filter((el) => el.isDone)
+        .map((el) => (
+          <TodoList
+            updateStatus={updateStatus}
+            updateTodo={updateTodo}
+            el={el}
+            key={el.id}
+            deleteTodo={deleteTodo}
+          />
+        ))}
+    </div>
+  );
 };
 
 export default Todo;
--- src/component/TodoList/index.js
+++ src/component/TodoList/index.js
@@ -1,23 +1,63 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const TodoList = ({el,deleteTodo}) => {
-    return (
-        <div>
-            <div    className='flex justify-between items-center bg-emerald-300 py-2 px-3 my-2 mx-3 '>
-                    <h1>{el.title}</h1>
-                    <div className='flex pt-2'>
-                    <button type="button" class="focus:outline-none pl-3  text-white bg-yellow-400 hover:bg-yellow-500  rounded-lg h-10  px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900"
-                    >Edit</button>
+const TodoList = ({ el, deleteTodo, updateTodo, updateStatus }) => {
+  const [edit, setEdit] = useState(false);
+  const [newValue, setNewValue] = useState(el.title);
 
-                    <button 
-                    onClick={()=>deleteTodo(el.id)}
-                    className='top-[10px]'
-                    type="button" class="focus:outline-none text-white bg-red-600 hover:bg-red-800 focus:ring-4font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-                    >Deleted</button>
-                    </div>
-                </div>
+  const handleChangeNew = (event) => {
+    setNewValue(event.target.value);
+  };
+
+  const openInput = () => {
+    setEdit(true);
+  };
+  const closeInput = (id, newTitle) => {
+    updateTodo(id, newTitle);
+    setEdit(false);
+  };
+
+  return (
+    <div className="w-[500px] bg-amber-400 py-4 px-3 mx-auto rounded-1 ">
+      <div className="flex justify-between items-center bg-emerald-300 py-2 px-3 my-0 mx-3 ">
+        <div className="flex items-center gap-2">
+          <input
+            type="checkbox"
+            defaultChecked={el.isDone}
+            onClick={() => updateStatus(el.id)}
+          />
+          {edit ? (
+            <input
+              className="py-2 px-2 rounded outline-none"
+              onChange={handleChangeNew}
+              defaultValue={el.title}
+              type="text"
+            />
+          ) : (
+            <h1>{el.title}</h1>
+          )}
         </div>
-    );
+
+        <div className="flex pt-2">
+          <button
+            onClick={() => (edit ? closeInput(el.id, newValue) : openInput())}
+            type="button"
+            class="focus:outline-none pl-3  text-white bg-yellow-400 hover:bg-yellow-500  rounded-lg h-10  px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900"
+          >
+            {edit ? "save" : "edit"}
+          </button>
+
+          <button
+            onClick={() => deleteTodo(el.id)}
+            className="top-[10px]"
+            type="button"
+            class="focus:outline-none text-white bg-red-600 hover:bg-red-800 focus:ring-4font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+          >
+            Deleted
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TodoList;
